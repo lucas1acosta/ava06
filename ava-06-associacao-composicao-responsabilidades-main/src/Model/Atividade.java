793d0ab113008008c7f2c45dec89065b43986b29
lucas1acosta <90867184+lucas1acosta@users.noreply.github.com>
2021-12-17 00:20:01 -0300

    Add files via upload

---
+++ ava-06-associacao-composicao-responsabilidades-main/src/Model/Atividade.java
@@ -0,0 +1,21 @@
+package Model;
+
+public class Atividade {
+
+    public Object getDescricao() {
+        return null;
+    }
+
+    public int getVagas() {
+        return 0;
+    }
+
+    public Object getTipo() {
+        return null;
+    }
+
+    public int getAno() {
+        return 0;
+    }
+    
+}
